5d27544df55c28db16d37018afc4fef0d17815fa
Brie Bencich <bbencich@hotmail.com>
2020-12-13 19:27:19 +0200

    push

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,5 +1,4 @@
 async function commentFormHandler(event) {
-<<<<<<< HEAD
     event.preventDefault();
   
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
@@ -29,37 +28,3 @@ async function commentFormHandler(event) {
   
   document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
   
-=======
-  event.preventDefault();
-
-  const comment_text = document
-    .querySelector('textarea[name="comment-body"]')
-    .value.trim();
-  const order_id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ];
-
-  if (comment_text) {
-    const response = await fetch("/api/comments", {
-      method: "POST",
-      body: JSON.stringify({
-        order_id,
-        comment_text,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
-
-document
-  .querySelector(".comment-form")
-  .addEventListener("submit", commentFormHandler);
->>>>>>> e02694bab5c96cb6802f8616f45a0d2485c15929
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -12,31 +11,10 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-=======
-const path = require("path");
-const express = require("express");
-const exphbs = require("express-handlebars");
-const routes = require("./controllers/");
-const sequelize = require("./config/connection");
-const session = require("express-session");
-const helpers = require("./utils/helpers");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-const hbs = exphbs.create({ helpers });
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const sess = {
-  secret: "Super secret secret",
->>>>>>> e02694bab5c96cb6802f8616f45a0d2485c15929
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-<<<<<<< HEAD
     db: sequelize
   })
 };
@@ -59,24 +37,3 @@ app.use(require('./controllers/'));
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-=======
-    db: sequelize,
-  }),
-};
-
-app.use(session(sess));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-});
-
-// turn on connection to database and server
-// sequelize.sync({ force: true }).then(() => {
-//   app.listen(PORT, () => console.log("Now listening"));
-// });
->>>>>>> e02694bab5c96cb6802f8616f45a0d2485c15929
