6f7f35010372a97e6d2bc5d3db2e78b8dfb840a1
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-12-14 12:23:59 -0500

    updating handlebars & CSS from brie's branch & updating homepage graphic to feature slogan

---
+++ public/javascript/delete-order.js
@@ -0,0 +1,20 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+  
+    const id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+    const response = await fetch(`/api/orders/${id}`, {
+      method: "DELETE",
+    });
+  
+    if (response.ok) {
+      document.location.replace("/dashboard/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document
+    .querySelector(".delete-order-btn")
+    .addEventListener("click", deleteFormHandler);
