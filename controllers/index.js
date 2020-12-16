bc3e097f0b4605da4619a8262d360ad0f0ca80f5
Lyly Long <lylylong@gmail.com>
2020-12-16 03:47:53 -0500

    fix navbar-collapse, change loading route, revise stylesheet

--- controllers/index.js
+++ controllers/index.js
@@ -5,9 +5,9 @@ const homeRoutes = require("./home-routes.js");
 const dashboardRoutes = require("./dashboard-routes.js");
 const loadingRoutes = require("./loading-routes.js");
 
-router.use("/", homeRoutes);
+router.use("/gallery", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
-router.use("/loading", loadingRoutes);
+router.use("/", loadingRoutes);
 
 module.exports = router;
--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -5,7 +5,7 @@ async function logout() {
   });
 
   if (response.ok) {
-    document.location.replace("/loading");
+    document.location.replace("/");
   } else {
     alert(response.statusText);
   }
